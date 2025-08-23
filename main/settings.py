e0f170075d6f3cf84399791554df71429e8ca700
João Pedro Limão <jplimao077@gmail.com>
2025-08-23 14:36:13 -0300

    change log functions

--- main/settings.py
+++ main/settings.py
@@ -1,4 +1,6 @@
 import os
+from selenium.webdriver.chrome.options import Options
+import inspect
 
 URL: str = "https://www.fundsexplorer.com.br/ranking"
 TIME_TO_SLEEP: int = 10
@@ -6,9 +8,34 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class Log():
+    def _get_caller_name()-> str:
+        caller_frame = inspect.stack()[2].frame
+        caller_function = inspect.stack()[2].function
+        caller_self = caller_frame.f_locals.get('self', None)
+        if caller_self:
+            return f"[{caller_self.__class__.__name__}] {caller_function.upper()}"
+        else:
+            return f"[No Class] {caller_function.upper()}"
+
     def log(tag: str, msg: str):
-        print(f'{tag}: {msg}')
+        caller = Log._get_caller_name()
+        print(f'{caller}: {msg}')
     
     def log_error(tag: str, msg: str, error: Exception):
-        print(f'{tag}: {msg}')
+        caller = Log._get_caller_name()
+        print(f'{caller}: {msg}')
         print(f'Root cause: {error}')
+
+
+class Selenium():
+
+    @staticmethod
+    def get_options() -> Options:
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+        return chrome_options
