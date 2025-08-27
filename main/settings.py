42cec49405c712e195fc389265e1d713746b648b
João Pedro Limão <jplimao077@gmail.com>
2025-08-27 20:57:15 -0300

    create function check_if_file_was_downloaded

--- main/settings.py
+++ main/settings.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 from selenium.webdriver.chrome.options import Options
 import inspect
 
@@ -33,10 +33,29 @@ class Selenium():
     @staticmethod
     def get_options() -> Options:
         options = Options()
-        # options.add_argument("--headless=new")
+        options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
         options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+        prefs = {
+            "download.default_directory": BASE_DIR_DOWNLOAD,
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "safebrowsing.enabled": True,
+        }
+        options.add_experimental_option('prefs', prefs)
         return options
+
+
+def check_if_file_was_downloaded(filename: str, timeout: int) -> bool:
+    found = False
+    for _ in range(timeout):
+        files = [f for f in os.listdir(BASE_DIR_DOWNLOAD) if f.endswith(filename)]
+        if files:
+            found = True
+            break
+        time.sleep(1)
+
+    return found
