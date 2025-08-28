61cc6e71680d22494f290b451e5cf350512425b7
João Pedro Limão <jplimao077@gmail.com>
2025-08-28 19:23:33 -0300

    finish trading view service

--- main/main.py
+++ main/main.py
@@ -1,6 +1,7 @@
 from services.funds_explorer import FundsExplorerService
 from services.status_invest import StatusInvestService
 from services.market_data import MarketDataService
+from services.trading_view import TradingViewService
 
 def get_funds():
     service = FundsExplorerService()
@@ -13,4 +14,8 @@ def get_stocks():
     market_data_service = MarketDataService()
     market_data_service.run()
 
+    stocks = ["BBAS3", "BEEF3", "BIDI4"]
+    service = TradingViewService()
+    service.run(stocks)
     # TODO: Join all data into one csv
+
--- services/trading_view.py
+++ services/trading_view.py
@@ -3,39 +3,61 @@ from typing import List
 import requests
 from requests import Response
 from bs4 import BeautifulSoup, Tag
-from main.settings import HTTP_SUCCESS_CODE, Log
+from main.settings import HTTP_SUCCESS_CODE, Log, BASE_DIR_DOWNLOAD
 
 class TradingViewService(Service):
 
     _SYMBOL = ":stock:"
     _URL = f"https://br.tradingview.com/symbols/BMFBOVESPA-{_SYMBOL}/"
-    _IMG_SEARCH_STRING = 'img[class*="logo-"]'
-    _IMG_WANTED_ATTRIBUTE  = "src"
+    _FILE_PATH = f"{BASE_DIR_DOWNLOAD}/trading-view-stocks.csv"
 
     def config_step(self):
-        self.url = self._URL.replace(":stock:", self.stock)
+        Log.log("Start")
+        HEADER = "STOCK;NAME;DEPARTMENT;IMAGE\n"
+        Log.log("Open file")
+        self.file = open(self._FILE_PATH, "a", encoding="utf-8")
+        self.file.write(HEADER)
+        self.lines = []
 
     def make_request(self):
-        response: Response = requests.get(self.url)
+        url = self._URL.replace(self._SYMBOL, self.stock)
+        response: Response = requests.get(url)
+
         if response.status_code == HTTP_SUCCESS_CODE:
             html = response.text
             self.page = BeautifulSoup(html, 'html.parser')
+            self.execute_next_step = True
         else:
-            Log.log(f"Failed request with status code {response.status_code}")
+            Log.log(f"Failed request with status code {response.status_code} when call for {self.stock}")
+            self.execute_next_step = False
 
     def read_page_and_get_data(self):
-            img_tag: Tag = self.page.select_one(self._IMG_SEARCH_STRING)
+            if not self.execute_next_step:
+                Log.log("Skip step")
+                return 
 
-            if img_tag == None:
-                Log.log(f"It is not possible find tag")
-            
-            image = img_tag[self._IMG_WANTED_ATTRIBUTE]
-            name_tag: Tag = self.page.select_one(self._)
+            img_tag = self.page.select_one('img[class*="logo-"]')            
+            image = img_tag["src"]
+
+            name_tag = self.page.find("h1", class_="apply-overflow-tooltip title-HDE_EEoW")
+            name = name_tag.get_text(strip=True)
+
+            prefix = "/markets/stocks-brazil/sectorandindustry-sector/"
+            department_tag = self.page.select_one(f'a[href^="{prefix}"]')  
+            department = department_tag.get_text(strip=True)
+
+            self.lines.append(f"{self.stock};{name};{department};{image}\n")
 
     def transform_data_into_csv(self):
-        pass
+        Log.log("Writing lines")
+        self.file.writelines(self.lines)
+        Log.log("Close file")
+        self.file.close()
 
     def run(self, stocks: List[str]):
+        self.config_step()
         for stock in stocks:
             self.stock = stock
-            super().run()
+            self.make_request()
+            self.read_page_and_get_data()
+        self.transform_data_into_csv()
