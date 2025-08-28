fcec992dda564cc221a1d95ed77cb43f08f7fb9a
João Pedro Limão <jplimao077@gmail.com>
2025-08-27 21:00:14 -0300

    create new script

--- main/main.py
+++ main/main.py
@@ -1,9 +1,16 @@
 from services.funds_explorer import FundsExplorerService
 from services.status_invest import StatusInvestService
+from services.market_data import MarketDataService
 
 def get_funds():
-    service = StatusInvestService()
+    service = FundsExplorerService()
     service.run()
 
+def get_stocks():
+    status_invest_service = StatusInvestService()
+    status_invest_service.run()
 
+    market_data_service = MarketDataService()
+    market_data_service.run()
 
+    # TODO: Join all data into one csv
